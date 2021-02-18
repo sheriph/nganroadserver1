6155899de3aed9cb9b1a9a016a7815379fc285e9
sheriph <sheriph4real@gmail.com>
2021-02-18 20:32:41 +0100

    update

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 // create a GET route
 app.get("/express_backend", (req, res) => {
   res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
